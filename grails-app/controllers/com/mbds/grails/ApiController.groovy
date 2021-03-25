05325599ac6c0b5b71d6d7bc2929ecbc0fe66108
Tolotriniavo <tolotriniavo.ralaiarisoa@gmail.com>
2021-03-25 16:47:09 +0300

    api
    vita le api

--- grails-app/controllers/com/mbds/grails/ApiController.groovy
+++ grails-app/controllers/com/mbds/grails/ApiController.groovy
@@ -2,6 +2,7 @@ package com.mbds.grails
 
 import grails.converters.JSON
 import grails.converters.XML
+import grails.plugin.springsecurity.SpringSecurityService
 import grails.plugin.springsecurity.annotation.Secured
 
 import javax.servlet.http.HttpServletResponse
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServletResponse
 @Secured(['ROLE_ADMIN'])
 class ApiController {
     AnnonceService annonceService
+    UserService userService
+    SpringSecurityService springSecurityService
 
 //    GET / PUT / PATCH / DELETE
 //    url : localhost:8081/projet/api/annonce(s)/{id}
@@ -20,19 +23,37 @@ class ApiController {
                 def annonceInstance = Annonce.get(params.id)
                 if (!annonceInstance)
                     return response.status = HttpServletResponse.SC_NOT_FOUND
-//                response.withFormat {
-//                    xml { render annonceInstance as XML }
-//                    json { render annonceInstance as JSON }
-//                }
+                response.withFormat {
+                  xml { render annonceInstance as XML }
+                   json { render annonceInstance as JSON }
+                }
                 serializeData(annonceInstance, request.getHeader("Accept"))
                 break
             case "PUT":
+                if (!params.id)
+                    return response.status = HttpServletResponse.SC_BAD_REQUEST
+                def req = request.JSON
+
+                def annonce = Annonce.get(params.id)
+                annonce.title = req.title
+                println "params" + annonce.title
+
+                annonce.description = req.description
+                annonce.price = Double.parseDouble(req.price)
+                //annonce.author = User.get(params.author.id)
+
+                if (annonce == null) {
+                    notFound()
+                    return
+                }
+                annonceService.save(annonce)
+                return response.status = HttpServletResponse.SC_OK
                 break
             case "PATCH":
                 break
             case "DELETE":
                 if (!params.id)
-                    return response.status = HttpServletResponse.SC_BAD_REQUEST
+                return response.status = HttpServletResponse.SC_BAD_REQUEST
                 annonceService.delete(params.id)
                 return response.status = HttpServletResponse.SC_OK
                 break
@@ -45,17 +66,98 @@ class ApiController {
 
 //    GET / POST
     def annonces() {
-
+        switch (request.getMethod()) {
+            case "GET":
+                def annonceInstance = annonceService.list();
+                response.withFormat {
+                    xml { render annonceInstance as XML }
+                    json { render annonceInstance as JSON }
+                }
+                serializeData(annonceInstance, request.getHeader("Accept"))
+                break
+            case "POST":
+                def req = request.JSON
+                println "id"+springSecurityService.currentUser.id
+                def user = User.get(springSecurityService.currentUser.id)
+                def annonce =new Annonce(
+                        title: req.title,
+                        description: req.description,
+                        price: Double.parseDouble(req.price)
+                )
+                user.addToAnnonces(annonce)
+                user.save(flush: true, failOnError: true)
+                return response.status = HttpServletResponse.SC_OK
+                break
+        }
+        return response.status = HttpServletResponse.SC_NOT_ACCEPTABLE
     }
 
 //    GET / PUT / PATCH / DELETE
     def user() {
+        switch (request.getMethod()) {
+            case "GET":
+                if (!params.id)
+                    return response.status = HttpServletResponse.SC_BAD_REQUEST
+                def userInstance = User.get(params.id)
+                if (!userInstance)
+                    return response.status = HttpServletResponse.SC_NOT_FOUND
+                response.withFormat {
+                    xml { render userInstance as XML }
+                    json { render userInstance as JSON }
+                }
+                serializeData(userInstance, request.getHeader("Accept"))
+                break
+            case "PUT":
+                if (!params.id)
+                    return response.status = HttpServletResponse.SC_BAD_REQUEST
+                def req = request.JSON
 
+                def user = User.get(params.id)
+                if (user == null) {
+                    notFound()
+                    return
+                }
+                user.username = req.username
+                userService.save(user)
+                return response.status = HttpServletResponse.SC_OK
+                break
+            case "PATCH":
+                break
+            case "DELETE":
+                if (!params.id)
+                    return response.status = HttpServletResponse.SC_BAD_REQUEST
+                userService.delete(params.id)
+                return response.status = HttpServletResponse.SC_OK
+                break
+            default:
+                return response.status = HttpServletResponse.SC_METHOD_NOT_ALLOWED
+                break
+        }
+        return response.status = HttpServletResponse.SC_NOT_ACCEPTABLE
     }
 
 //    GET / POST
     def users() {
+        switch (request.getMethod()) {
+            case "GET":
+                def user = userService.list();
+                response.withFormat {
+                    xml { render user as XML }
+                    json { render user as JSON }
+                }
+                serializeData(user, request.getHeader("Accept"))
+                break
+            case "POST":
+                def req = request.JSON
+                def userUser = new User(username: req.username, password: req.password).save()
+                def userRole = Role.findByAuthority(req.role)
+                UserRole.create(userUser, userRole, true)
 
+
+                return response.status = HttpServletResponse.SC_OK
+                break
+        }
+        return response.status = HttpServletResponse.SC_NOT_ACCEPTABLE
     }
 
     def serializeData(object, format)
